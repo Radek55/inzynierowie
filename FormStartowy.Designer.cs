e4ea59f80e7f7f2cd6e01fb62068b54bdf3633ab
Frane1o <franek.dudek@wp.pl>
2022-10-23 17:55:35 +0200

    23.10

--- FormStartowy.Designer.cs
+++ FormStartowy.Designer.cs
@@ -31,35 +31,120 @@ namespace AplikacjaInzynierka.Wprowadzenie
         {
             this.WprowadzenieTekst = new System.Windows.Forms.Label();
             this.WprowadzeniePrzycisk = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.fontDialog2 = new System.Windows.Forms.FontDialog();
+            this.kod = new System.Windows.Forms.TextBox();
+            this.instrukcja = new System.Windows.Forms.Label();
+            this.dalej = new System.Windows.Forms.Button();
+            this.nagranie1 = new System.Windows.Forms.Label();
+            this.nagranie2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // WprowadzenieTekst
             // 
             this.WprowadzenieTekst.AutoSize = true;
-            this.WprowadzenieTekst.Location = new System.Drawing.Point(100,100);
+            this.WprowadzenieTekst.Location = new System.Drawing.Point(54, 85);
+            this.WprowadzenieTekst.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.WprowadzenieTekst.Name = "WprowadzenieTekst";
-            this.WprowadzenieTekst.Size = new System.Drawing.Size(356, 128);
+            this.WprowadzenieTekst.Size = new System.Drawing.Size(176, 105);
             this.WprowadzenieTekst.TabIndex = 1;
-            this.WprowadzenieTekst.Text = "Siema, tutaj tekst wprowadzenia\r\nw\r\nw\r\nw";
+            this.WprowadzenieTekst.Text = "Siema, tutaj tekst wprowadzenia\r\nw\r\nw\r\nw\r\n\r\n\r\nPodaj kod użytkownika";
             this.WprowadzenieTekst.Click += new System.EventHandler(this.label1_Click);
             // 
             // WprowadzeniePrzycisk
             // 
-            this.WprowadzeniePrzycisk.Location = new System.Drawing.Point(1000,575);
+            this.WprowadzeniePrzycisk.Location = new System.Drawing.Point(538, 270);
+            this.WprowadzeniePrzycisk.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.WprowadzeniePrzycisk.Name = "WprowadzeniePrzycisk";
-            this.WprowadzeniePrzycisk.Size = new System.Drawing.Size(150,50);
+            this.WprowadzeniePrzycisk.Size = new System.Drawing.Size(81, 23);
             this.WprowadzeniePrzycisk.TabIndex = 2;
             this.WprowadzeniePrzycisk.Text = "Start";
             this.WprowadzeniePrzycisk.UseVisualStyleBackColor = true;
             this.WprowadzeniePrzycisk.Click += new System.EventHandler(this.WprowadzeniePrzycisk_Click);
             // 
+            // kod
+            // 
+            this.kod.Location = new System.Drawing.Point(54, 193);
+            this.kod.Name = "kod";
+            this.kod.Size = new System.Drawing.Size(236, 23);
+            this.kod.TabIndex = 3;
+            // 
+            // instrukcja
+            // 
+            this.instrukcja.AutoSize = true;
+            this.instrukcja.Location = new System.Drawing.Point(54, 85);
+            this.instrukcja.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.instrukcja.Name = "instrukcja";
+            this.instrukcja.Size = new System.Drawing.Size(83, 15);
+            this.instrukcja.TabIndex = 4;
+            this.instrukcja.Text = "tekst instrukcji\r\n";
+            // 
+            // dalej
+            // 
+            this.dalej.Location = new System.Drawing.Point(538, 270);
+            this.dalej.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
+            this.dalej.Name = "dalej";
+            this.dalej.Size = new System.Drawing.Size(81, 23);
+            this.dalej.TabIndex = 5;
+            this.dalej.Text = "Dalej";
+            this.dalej.UseVisualStyleBackColor = true;
+            this.dalej.Click += new System.EventHandler(this.dalej_Click);
+            // 
+            // nagranie1
+            // 
+            this.nagranie1.AutoSize = true;
+            this.nagranie1.Location = new System.Drawing.Point(166, 85);
+            this.nagranie1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nagranie1.Name = "nagranie1";
+            this.nagranie1.Size = new System.Drawing.Size(64, 15);
+            this.nagranie1.TabIndex = 6;
+            this.nagranie1.Text = "Nagranie 1";
+            // 
+            // nagranie2
+            // 
+            this.nagranie2.AutoSize = true;
+            this.nagranie2.Location = new System.Drawing.Point(414, 85);
+            this.nagranie2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.nagranie2.Name = "nagranie2";
+            this.nagranie2.Size = new System.Drawing.Size(64, 15);
+            this.nagranie2.TabIndex = 7;
+            this.nagranie2.Text = "Nagranie 2";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(438, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(185, 176);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
             // FormStartowy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 675);
+            this.ClientSize = new System.Drawing.Size(635, 310);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.nagranie2);
+            this.Controls.Add(this.nagranie1);
+            this.Controls.Add(this.dalej);
+            this.Controls.Add(this.instrukcja);
+            this.Controls.Add(this.kod);
             this.Controls.Add(this.WprowadzeniePrzycisk);
             this.Controls.Add(this.WprowadzenieTekst);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "FormStartowy";
             this.Text = "Badanie";
             this.ResumeLayout(false);
@@ -71,5 +156,14 @@ namespace AplikacjaInzynierka.Wprowadzenie
 
         private Label WprowadzenieTekst;
         private Button WprowadzeniePrzycisk;
+        private FontDialog fontDialog1;
+        private FontDialog fontDialog2;
+        private TextBox kod;
+        private Label instrukcja;
+        private Button dalej;
+        private Label nagranie1;
+        private Label nagranie2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
--- FormStartowy.cs
+++ FormStartowy.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,9 +14,14 @@ namespace AplikacjaInzynierka.Wprowadzenie
 {
     public partial class FormStartowy : Form
     {
+        public char[] wynik;
+        public char wynikt;
         public FormStartowy()
         {
             InitializeComponent();
+            instrukcja.Hide();
+            dalej.Hide();
+            glosowanie();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -25,12 +31,59 @@ namespace AplikacjaInzynierka.Wprowadzenie
 
         private void WprowadzeniePrzycisk_Click(object sender, EventArgs e)
         {
-            ZapisDanych zd = new ZapisDanych();
-            zd.ZapisLokalnie();
-            UserControl control = new UserControl();
-            control.Dock = DockStyle.Fill;
-            this.Controls.Add(control);
+            string a = kod.Text;
+            /* if (int.Parse(a) > 0)
+             {
+                 WprowadzenieTekst.Text = a;
+             }
+             else
+             {
+
+             }*/
+            WprowadzenieTekst.Hide();
+            kod.Hide();
+            WprowadzeniePrzycisk.Hide();
+            instrukcja.Show();
+            dalej.Show();
         }
 
+        private void dalej_Click(object sender, EventArgs e)
+        {
+            instrukcja.Hide();
+            playSimpleSound("sinus_2s.wav");
+        }
+
+        private void playSimpleSound(string plik)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\frane\Desktop\praca inżynierska 19.10.22\baza nagrań\prosty sinus\"+ plik);
+            simpleSound.Play();
+        }
+
+        private void glosowanie()
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            
+            checkBox1.CheckedChanged += CheckBox1_CheckedChanged;
+            checkBox2.CheckedChanged += CheckBox2_CheckedChanged;
+        }
+
+        private void CheckBox1_CheckedChanged(object? sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                checkBox2.Checked = false;
+                wynikt = '0';
+                wynikt = '1';
+            }
+        }
+        private void CheckBox2_CheckedChanged(object? sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                checkBox1.Checked = false;
+
+            }
+        }
     }
 }
